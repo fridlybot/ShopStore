80d11eaae35227fd3db3172b251b7d67f1b5f2dd
fridlybot <146335388+fridlybot@users.noreply.github.com>
2023-11-03 17:22:25 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const path = require('path');
 
-app.use(express.static('public'));
+// Serve the static files (HTML, CSS, JS) from the current directory
+app.use(express.static(__dirname));
 
+// Define a route to serve the index.html file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Start the server
+const port = process.env.PORT || 3000; // You can use any available port
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
